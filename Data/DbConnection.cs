c747282ab37d91062ca726bbd406a6fc488ff92a
romanafa <r.faiglova@gmail.com>
2025-10-29 10:15:11 +0100

    Connection string for database

---
+++ Data/DbConnection.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql;
+
+namespace TelemetryWebApp.Data
+{
+    public class DbConnection
+    {
+        private string _connectionString;
+
+        public DbConnection()
+        {
+            _connectionString = "Host=ider-database.westeurope.cloudapp.azure.com;Port=5433;Username=astro;Password=;Database=astro";
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+    }
+}
