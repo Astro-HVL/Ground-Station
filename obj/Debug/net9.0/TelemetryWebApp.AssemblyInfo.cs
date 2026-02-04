fa301efe4b39e24e456397d75593c21ba63444a6
bendikbp <48202511+bendikbp@users.noreply.github.com>
2026-02-04 15:44:15 +0100

    gammel versjon

--- obj/Debug/net9.0/TelemetryWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TelemetryWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e7559ae94554401a805b51a99240e2a4ca66a72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fbf8f3ab4528ead58ea916478191829d9142c05")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
