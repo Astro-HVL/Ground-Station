982cdb9d3b01720acf792842e6690f3cf3097137
unknown <linnestad44@gmail.com>
2026-02-25 11:47:13 +0100

    Definert svart boks i html

--- obj/Debug/net9.0/TelemetryWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TelemetryWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101858dcb7cf2808c39d0d3a4ee41e738ada56b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81b79cac0efe2981b23eed32eccff7fe79d898c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
