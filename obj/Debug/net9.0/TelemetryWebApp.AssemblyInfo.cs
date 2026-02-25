4296b3e0ad42f2b21402f37ff45d60e9f7a5f0e0
unknown <linnestad44@gmail.com>
2026-02-25 15:38:04 +0100

    Lagt til sponsorer til livedata.html

--- obj/Debug/net9.0/TelemetryWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/TelemetryWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982cdb9d3b01720acf792842e6690f3cf3097137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f82fe869cbe525decc40488a798822b8e11abc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelemetryWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
